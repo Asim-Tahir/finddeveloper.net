7438516d30b2fee56913763b48e29263f45f185f
Asım Tahir <AsimTahir@protonmail.com>
2023-09-23 22:31:40 +0300

    refactor: use `Mark Pro` as global font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,16 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin-ext"] });
+import { markPro } from "@/assets/fonts";
 
 export const metadata: Metadata = {
   title: "Find Developer",
   description: "find your teammate",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={markPro.className}>{children}</body>
     </html>
   );
 }
