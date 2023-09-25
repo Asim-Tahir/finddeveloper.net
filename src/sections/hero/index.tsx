cb2c6a7ccdff7abf8c829a54fbe14d02a2f910e9
Asım Tahir <AsimTahir@protonmail.com>
2023-09-25 21:49:39 +0300

    refactor: complete hero section

--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -8,73 +8,98 @@ import styles from "./styles.module.css";
 import heroBackgroundImage from "@/assets/svgs/hero-background.svg";
 import logoImage from "@/assets/svgs/logo.svg";
 import heroTextImage from "@/assets/svgs/hero-text.svg";
+import heroSearchButtonIcon from "@/assets/svgs/hero-search-button-icon.svg";
+import heroSearchInputIcon from "@/assets/svgs/hero-search-input-icon.svg";
 
 export default function Hero(): JSX.Element {
   function onSearchFormSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    const formdata = new FormData(event.target as HTMLFormElement);
+    const form: HTMLFormElement = event.target as HTMLFormElement;
+
+    const formdata = new FormData(form);
     const searchText = formdata.get("search");
+
+    if (!searchText || (typeof searchText === "string" && !searchText.trim())) {
+      return;
+    }
 
     console.log("searchText:", searchText);
   }
 
   return (
     <section
-      className={styles.hero}
+      className={styles.heroWrapper}
       style={{
         "--bg-image-url": `url(${heroBackgroundImage.src})`,
         "--bg-image-width": heroBackgroundImage.width,
         "--bg-image-height": heroBackgroundImage.height,
-        "--bg-image-blur-data-url": `url(${heroBackgroundImage.blurDataURL})`,
       }}
     >
-      <header className={styles.heroHeader}>
-        <Link className={styles.heroAuthButton} href="/profile">
-          Kayıt Ol
-        </Link>
-        <Link className={styles.heroAuthButton} href="/profile">
-          Giriş Yap
-        </Link>
-      </header>
-
-      <Link href="/">
-        <Image
-          className={styles.heroLogo}
-          src={logoImage.src}
-          width={logoImage.width}
-          height={logoImage.height}
-          blurDataURL={logoImage.blurDataURL}
-          alt="finddeveloper.net Logo"
-        />
-      </Link>
-
-      <Image
-        className={styles.heroText}
-        src={heroTextImage.src}
-        width={heroTextImage.width}
-        height={heroTextImage.height}
-        blurDataURL={heroTextImage.blurDataURL}
-        alt="finddeveloper.net slogan that said 'find your teammate'"
-      />
-
-      <form className={styles.heroSearchForm} onSubmit={onSearchFormSubmit}>
-        <input
-          className={styles.heroSearchInput}
-          name="search"
-          type="search"
-          inputMode="search"
-          placeholder="İş Ara | İş, Şirket, Anahtar Kelime"
-        />
-        <div className={styles.heroSearchButtonGroup}>
-          <button className={styles.heroSearchButton}>
-            Find{/* TODO: this should be image */}
-          </button>
-          <Link className={styles.heroSearchBottomText} href="#">
-            Detaylı Arama
+      <div className={styles.hero}>
+        <header className={styles.heroHeader}>
+          <Link className={styles.heroAuthButton} href="/profile">
+            Kayıt Ol
           </Link>
-        </div>
-      </form>
+          <Link className={styles.heroAuthButton} href="/profile">
+            Giriş Yap
+          </Link>
+        </header>
+
+        <Link href="/" className={styles.heroLogoLink}>
+          <Image
+            className={styles.heroLogo}
+            src={logoImage.src}
+            width={logoImage.width}
+            height={logoImage.height}
+            alt="finddeveloper.net Logo"
+          />
+        </Link>
+
+        <Image
+          className={styles.heroText}
+          src={heroTextImage.src}
+          width={heroTextImage.width}
+          height={heroTextImage.height}
+          alt="finddeveloper.net slogan that said 'find your teammate'"
+        />
+
+        <form className={styles.heroSearchForm} onSubmit={onSearchFormSubmit}>
+          <label className={styles.heroSearchInputLabel}>
+            <Image
+              className={styles.heroSearchInputIcon}
+              src={heroSearchInputIcon.src}
+              width={heroSearchInputIcon.width}
+              height={heroSearchInputIcon.height}
+              alt="finddeveloper.net search input before icon"
+            />
+            <input
+              className={styles.heroSearchInput}
+              name="search"
+              type="search"
+              required
+              autoComplete="off"
+              inputMode="search"
+              placeholder="İş Ara | İş, Şirket, Anahtar Kelime"
+            />
+          </label>
+
+          <div className={styles.heroSearchButtonGroup}>
+            <button className={styles.heroSearchButton}>
+              <Image
+                src={heroSearchButtonIcon.src}
+                width={heroSearchButtonIcon.width}
+                height={heroSearchButtonIcon.height}
+                alt="finddeveloper.net search icon"
+              />
+            </button>
+
+            <Link className={styles.heroSearchBottomText} href="#">
+              Detaylı Arama
+            </Link>
+          </div>
+        </form>
+      </div>
     </section>
   );
 }
