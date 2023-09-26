a270c6e0c3789872f0cc8f66b13b3efcc13d7074
Asım Tahir <AsimTahir@protonmail.com>
2023-09-27 01:18:22 +0300

    feat: add `popular-searches` section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import styles from "./page.module.scss";
 
 import Hero from "@/sections/hero";
+import PopularSearches, { SearchRecord } from "@/sections/popular-searches";
+
+import popularSearches from "@/mocks/popular-searches.json";
 
 export default function HomePage() {
   return (
     <main className={styles.main}>
       <Hero />
+      <PopularSearches
+        title="Popüler aramalar"
+        searches={popularSearches as Array<SearchRecord>}
+      />
     </main>
   );
 }
