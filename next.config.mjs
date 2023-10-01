2f7dfc51b1e4d1dfceece9e0b684126be0e672a1
Asım Tahir <AsimTahir@protonmail.com>
2023-10-01 14:33:25 +0300

    refactor: extend typing of next config for `sassOptions`

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig & {sassOptions: import('sass').Options<"sync">}} */
+import { fileURLToPath, URL } from "url";
+
+/**
+ * @typedef {import('next').NextConfig & {
+ *   sassOptions: import('sass-loader').Options["sassOptions"] & {
+ *     includePaths: import('sass').LegacySharedOptions<"sync">["includePaths"],
+ *     prependData: import('sass-loader').Options["additionalData"],
+ *     additionalData: import('sass-loader').Options["additionalData"]
+ *   }
+ * }} NextConfig
+ *
+ * @type {NextConfig}
+ */
 const nextConfig = {
   reactStrictMode: false,
 };
