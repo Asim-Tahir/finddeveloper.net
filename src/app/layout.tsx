7179f9f48e4758e69c9d33eb6da39dc97247fe70
Asım Tahir <AsimTahir@protonmail.com>
2023-10-03 22:00:27 +0300

    feat: wrap root layout with redux provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/assets/styles/globals.scss";
 
+import { ReduxProvider } from "@/store";
 import { markPro } from "@/assets/fonts";
 
 import type { Metadata, NextLayoutProps } from "next";
@@ -12,7 +13,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: NextLayoutProps): JSX.Element {
   return (
     <html lang="tr">
-      <body className={markPro.className}>{children}</body>
+      <body className={markPro.className}>
+        <ReduxProvider>
+          {children}
+        </ReduxProvider>
+      </body>
     </html>
   );
 }
