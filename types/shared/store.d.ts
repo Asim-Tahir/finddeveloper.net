25e8aa221ed12e34787ee1cf74354d5aac42374b
Asım Tahir <AsimTahir@protonmail.com>
2023-10-04 01:12:52 +0300

    feat: add global ns for `user` store

--- types/shared/store.d.ts
+++ types/shared/store.d.ts
@@ -1,4 +1,4 @@
-import type { Action } from "redux";
+import type * as Redux from "redux";
 import type * as ReduxThunk from "redux-thunk";
 
 export = ReduxStore;
@@ -8,11 +8,14 @@ declare namespace ReduxStore {
   declare namespace RootStore {
     declare interface State {
       counter: CounterStore.State;
+      user: UserStore.State;
     }
 
-    declare type ActionType = CounterStore.ActionType;
+    declare type ActionType =
+      | CounterStore.ActionType
+      | UserStore.ActionType;
 
-    declare type Action = CounterStore.Action;
+    declare type Action = CounterStore.Action | UserStore.Action;
 
     declare type ThunkActionCreator<
       ReturnType = void,
@@ -20,20 +23,42 @@ declare namespace ReduxStore {
     > = ReduxThunk.ThunkAction<ReturnType, State, ExtraArgument, Action>;
   }
 
+  declare namespace UserStore {
+    declare interface State {
+      currentUser: User.Record;
+      loggedIn: boolean;
+      loading: boolean;
+    }
+
+    declare interface LoginActionPayload {
+      username: string;
+      password: string;
+    }
+
+    declare interface LoginAction extends Redux.AnyAction {
+      type: "user/login";
+      payload: User.Record;
+    }
+
+    declare type ActionType = Pick<LoginAction, "type">;
+
+    declare type Action = LoginAction;
+  }
+
   declare namespace CounterStore {
     declare interface State {
       value: number;
     }
 
-    declare interface IncrementAction extends Action {
+    declare interface IncrementAction extends Redux.AnyAction {
       type: "counter/increment";
     }
 
-    declare interface DecrementAction extends Action {
+    declare interface DecrementAction extends Redux.AnyAction {
       type: "counter/decrement";
     }
 
-    declare interface RandomAction extends Action {
+    declare interface RandomAction extends Redux.AnyAction {
       type: "counter/randomize";
       payload: number;
     }
